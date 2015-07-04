2a7e113dab353d5f51c315c91f0be04303029019
billperegoy <bill@billperegoy.com>
2015-07-04 16:45:56 -0400

    Added validations for all training plan fields.

--- app/models/training_plan.rb
+++ app/models/training_plan.rb
@@ -1,3 +1,5 @@
 class TrainingPlan < ActiveRecord::Base
-  validates_presence_of :name
+  validates :name, presence: true, uniqueness: true
+  validates :distance, presence: true, numericality: true
+  validates :weeks, presence: true, numericality: { only_integer: true }
 end
--- spec/models/training_plan_spec.rb
+++ spec/models/training_plan_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe TrainingPlan, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :name }
+  it { should validate_uniqueness_of :name }
+
+  it { should validate_presence_of :distance }
+  it { should validate_numericality_of :distance }
+
+  it { should validate_presence_of :weeks }
+  it { should validate_numericality_of(:weeks).only_integer }
 end
