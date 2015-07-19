d091f077c33bf69338a3c5e6495dc2ca13a67d7d
billperegoy <bill@billperegoy.com>
2015-07-19 19:38:33 -0400

    Added validation of user start_date

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true
+  validates :start_day, presence: true,
+    inclusion: { in: %w( Monday Tuesday Wednesday Thursday Friday Saturday Sunday )}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe User, :type => :model do
 
   it { should validate_presence_of :email }
   it { should validate_uniqueness_of :email }
+
+  it { should validate_presence_of :start_day }
+  it { should validate_inclusion_of(:start_day).
+    in_array(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']) }
 end
